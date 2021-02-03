8ad7da826aabe91bdf6080169b079cdcdfe1b587
meltem ayan <meltem.ayan96@hotmail.com>
2021-02-03 23:48:22 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic repository design pattern
+    //generic contraint
+    //class : referans tip constraint koyduk
+    //IEntity : referans tip ya IEntity ya da IEntity implemente eden bir nesne olabilir
+    //new() : new'lenebilir olmalıdır. IEntity newlenemediği için artık IEntity'yi I....Dal'larda kullanamayıze
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        //interface metodları default publictir
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    // Context : Db tablolarıile proje classlarını bağlamak
+    public class NorthwindContext:DbContext
+    {
+        //Db şurada diyoruz şuan override on conf seçiyoruz
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+
+        //hangi SQL tablosu hangi class'ı ifade ediyor:
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+
+    }
+}
